02e349bad87fae766c7829cb74c401836a16fe35
iseeyouwatching <leonidshibanovv@gmail.com>
2023-05-31 14:37:50 +0700

    Создал сущность пользователя

---
+++ src/main/java/ru/hits/musicservice/entity/User.java
@@ -0,0 +1,51 @@
+package ru.hits.musicservice.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import ru.hits.musicservice.enumeration.Gender;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(unique = true)
+    private String username;
+
+    private String name;
+
+    private String surname;
+
+    private String city;
+
+    private String country;
+
+    private int age;
+
+    private Gender gender;
+
+    private String bio;
+
+    private UUID avatar;
+
+}
---
+++ src/main/java/ru/hits/musicservice/enumeration/Gender.java
@@ -0,0 +1,9 @@
+package ru.hits.musicservice.enumeration;
+
+public enum Gender {
+
+    FEMALE,
+    MALE,
+    PREFER_NOT_TO_SAY
+
+}
