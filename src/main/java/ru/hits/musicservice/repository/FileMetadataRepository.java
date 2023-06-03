70517be47ad50f90237bd8c33b274a4defec2aef
iseeyouwatching <leonidshibanovv@gmail.com>
2023-06-03 14:25:03 +0700

    Добавил репозиторий для работы с сущностью метаданных файла

---
+++ src/main/java/ru/hits/musicservice/repository/FileMetadataRepository.java
@@ -0,0 +1,15 @@
+package ru.hits.musicservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.hits.musicservice.entity.FileMetadataEntity;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface FileMetadataRepository extends JpaRepository<FileMetadataEntity, UUID> {
+
+    Optional<FileMetadataEntity> findByObjectName(UUID id);
+
+}
